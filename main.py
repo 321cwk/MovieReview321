4e78f9db74564436f8dc12907ee0fda7ceedf2d5
sanyosoma <olliesarmies@gmail.com>
2015-02-20 10:33:49 +0000

    Got the movie search working

--- main.py
+++ main.py
@@ -1,50 +1,48 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import jinja
+import jinja2
 import webapp2
 import urllib2
 import json
+import os
  
-        
-title_to_search = raw_input("Give me a title: ")
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__),'templates')),
+    extensions={'jinja2.ext.autoescape'})
 
-omdb_api_url = "http://www.omdbapi.com/?t=" + title_to_search + '&y=&plot=short&r=json'
- 
-response = urllib2.urlopen(omdb_api_url).read()
- 
-data = json.loads(response.decode('utf8')) 
- 
-print 'Title:  ' + data['Title']
-print '-------------------'
-print 'Year:  ' + data['Year']
-print '-------------------'
-print 'Plot:  ' + data['Plot']
-print '-------------------'
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        title_to_search = 'matrix'
+        omdb_api_url = "http://www.omdbapi.com/?t=" + title_to_search + '&y=&plot=short&r=json'
+        response = urllib2.urlopen(omdb_api_url).read()
+        data = json.loads(response.decode('utf8'))
         
+        film = data['Title'] + '  ' + data['Year'] + '  ' + data['Plot']
         
+        template = JINJA_ENVIRONMENT.get_template('form.html')
+        template_values = {
+            'film': film,
+        }
+        self.response.write(template.render(template_values))
         
     def post(self):
         searchtitle = self.request.get('t')
         searchyear = self.request.get('y')
         searchplot = self.request.get('plot')
+        title_to_search = searchtitle
+        omdb_api_url = "http://www.omdbapi.com/?t=" + title_to_search + '&y=&plot=short&r=json'
+        response = urllib2.urlopen(omdb_api_url).read()
+        data = json.loads(response.decode('utf8'))
+        
+        film = data['Title'] + '  ' + data['Year'] + '  ' + data['Plot']
+        
+        template = JINJA_ENVIRONMENT.get_template('form.html')
+        template_values = {
+            'film': film,
+        }
+        self.response.write(template.render(template_values))
+        
         
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/movie', MainPage),
 ], debug=True)
